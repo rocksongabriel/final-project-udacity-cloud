8520e4a5eff5b2e539f0fe5e86dc8e6cfee3e2d4
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-05 14:02:58 +0000

    Change the code to function based code

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -9,17 +9,17 @@ const s3 = new AWS.S3({ signatureVersion: 'v4' })
 const EXPIRATION_TIME = process.env.SIGNED_URL_EXPIRATION
 const BUCKET_NAME = process.env.ATTACHEMENT_S3_BUCKET
 
-export class AttachmentUtils {
-  constructor() {}
+const createPresignedUrl = async (todoId: string): Promise<String> => {
+  const signedUrl = s3.getSignedUrl('putObject', {
+    Key: todoId,
+    Bucket: BUCKET_NAME,
+    Expires: EXPIRATION_TIME
+  })
 
-  async createPresignedUrl(todoId: string): Promise<String> {
-    const signedUrl = s3.getSignedUrl('putObject', {
-      Key: todoId,
-      Bucket: BUCKET_NAME,
-      Expires: EXPIRATION_TIME
-    })
+  return signedUrl
+}
 
-    return signedUrl
-  }
+export const AttachmentUtils = {
+  createPresignedUrl
 }
 
