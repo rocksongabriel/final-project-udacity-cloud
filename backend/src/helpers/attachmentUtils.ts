49bd1f435cc936178e7090a24df1d15c3cee2fe4
Gabriel Rockson <thegabrielrockson@gmail.com>
2022-09-05 13:24:21 +0000

    Complete the helper function for creating presigned url

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -4,3 +4,22 @@ import * as AWSXRay from 'aws-xray-sdk'
 const XAWS = AWSXRay.captureAWS(AWS)
 
 // TODO: Implement the fileStogare logic
+
+const s3 = new AWS.S3({ signatureVersion: 'v4' })
+const EXPIRATION_TIME = process.env.SIGNED_URL_EXPIRATION
+const BUCKET_NAME = process.env.ATTACHEMENT_S3_BUCKET
+
+export class AttachmentUtils {
+  constructor() {}
+
+  async createPresignedUrl(todoId: string): Promise<String> {
+    const signedUrl = s3.getSignedUrl('putObject', {
+      Key: todoId,
+      Bucket: BUCKET_NAME,
+      Expires: EXPIRATION_TIME
+    })
+
+    return signedUrl
+  }
+}
+
